8d51b9803a2d0421315087b2c825801c653a9ddb
Sunitha-QA <sunitha0902tg@gmail.com>
2025-03-02 18:30:54 -0600

    webhooks file added

---
+++ src/main/java/com/qa/opencart/listeners/NGROKWEBHOOKExample.java
@@ -0,0 +1,30 @@
+package com.qa.opencart.listeners;
+
+public class NGROKWEBHOOKExample {
+
+	
+	//dowload ngrok. unzip, add to path
+	//open terminal 
+	//got to where the ngrok is present
+	//enter : ngrok authtoken tokenvalue
+	// ngrok http 8080
+	
+	
+	//go to jenkins
+	//click on project settings
+	// click on webhooks
+	// click on add
+	//enter URL from terminal
+	// if connection established we get message in terminal 200OK
+	
+	//in jenkins
+	//in build triggers
+	//select the option "github hook trigger for polling"
+	
+	
+	//do git push origin master
+	//automatically project will run in  jenkins
+	
+	
+	
+}
